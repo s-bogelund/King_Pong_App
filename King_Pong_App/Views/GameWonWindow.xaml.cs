6f8e58740706ce935f41f24e1565b37cd5569122
simbj94 <61635370+simbj94@users.noreply.github.com>
2021-11-28 20:59:00 +0100

    Implemented and tested an end of game window. Added the KingPong logo the the background of all pop-up windows. Added a PrintInfo method to both Player- and TeamViewModel

---
+++ King_Pong_App/Views/GameWonWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using King_Pong_App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace King_Pong_App.Views
+{
+	/// <summary>
+	/// Interaction logic for GameWonWindow.xaml
+	/// </summary>
+	public partial class GameWonWindow : Window
+	{
+		public GameWonWindow()
+		{
+			InitializeComponent();
+			DisplayWinningTeam();
+			PlayerStats();
+		}
+
+		public void DisplayWinningTeam()
+		{
+			AnnounceWinnerLabel.Content = $"{App.currentTeam.Name.ToUpper()} VINDER!!";
+		}
+
+		public void PlayerStats()
+		{
+			WinningPlayerStats.Text = App.currentTeam.PrintInfo();
+			LosingPlayerStats.Text = App.currentTeam == App.team1 ? App.team1.PrintInfo() : App.team2.PrintInfo();
+			
+		}
+
+		private void GameOver_Click(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+	}
+}
